7ca6f4217ca6641d47f9e19309250a9bcca60657
kisun-sah <kishunsah783@gmail.com>
2024-08-08 17:40:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+ function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!  <hr />
+      I am student of sandip university .
+    </h1>
+  )
+}
+
+export default App;
